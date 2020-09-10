f4997a88285708fc87885fcd06b486c694906f33
Prismere <37064823+Prismere@users.noreply.github.com>
2020-09-10 14:32:55 +0530

    Login Image change, modaliyu, combo

--- src/main/java/com/apro/homescreen/Maincontroller.java
+++ src/main/java/com/apro/homescreen/Maincontroller.java
@@ -7,6 +7,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
@@ -18,14 +19,18 @@ public class Maincontroller {
 	@FXML	
 	AnchorPane newitem = new AnchorPane();
 	@FXML
-	public void viewinv()
+	public void addprod()
 	{
 		try
 		{
-		Parent root = FXMLLoader.load(getClass().getResource("/fxml/Items/category.fxml"));
+		Parent root = FXMLLoader.load(getClass().getResource("/fxml/Items/items.fxml"));
 		Scene scene = new Scene(root);
 		Stage stage = new Stage();
 		stage.setTitle("Inventory:: Version 1.0"); 
+		
+		/* make only foreground window active*/
+		stage.initModality(Modality.APPLICATION_MODAL);
+		
 		stage.initStyle(StageStyle.UNDECORATED);
 		stage.setScene(scene);
 		/* stage.setMaximized(true); */
--- src/main/java/com/apro/items/Itemcontroller.java
+++ src/main/java/com/apro/items/Itemcontroller.java
@@ -1,45 +1,47 @@
 package com.apro.items;
 
+import java.net.URL;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
+
+import org.controlsfx.control.textfield.TextFields;
 
 import com.apro.login.SqliteConnection;
+import com.sun.glass.events.KeyEvent;
 
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.stage.Stage;
 
-public class Itemcontroller {
+public class Itemcontroller implements Initializable{
 	Connection conn;
 
-	public Itemcontroller() {
-		conn = SqliteConnection.Connector();
-		if (conn == null) {
-			System.out.println("Connection Not Successful");
-
-			System.exit(1);
-		}
-	}
-	public boolean isDbConnected() {
-		try {
-			return !conn.isClosed();
-		} catch (SQLException e) {
-			e.printStackTrace();
-			return false;
-		}
-	}
-
-	public static Connection Connector() {
-		try {
-			Class.forName("org.sqlite.JDBC");
-			Connection conn1 = DriverManager.getConnection("jdbc:sqlite:testdb.sqlite");
-			return conn1;
-		} catch (Exception e) {
-			return null;
-		}
-	}
+	/*
+	 * public Itemcontroller() { conn = SqliteConnection.Connector(); if (conn ==
+	 * null) { System.out.println("Connection Not Successful");
+	 * 
+	 * System.exit(1); } } public boolean isDbConnected() { try { return
+	 * !conn.isClosed(); } catch (SQLException e) { e.printStackTrace(); return
+	 * false; } }
+	 * 
+	 * public static Connection Connector() { try {
+	 * Class.forName("org.sqlite.JDBC"); Connection conn1 =
+	 * DriverManager.getConnection("jdbc:sqlite:testdb.sqlite"); return conn1; }
+	 * catch (Exception e) { return null; } }
+	 */
 	@FXML
 	TextField pname = new TextField();
 	@FXML
@@ -68,9 +70,66 @@ public class Itemcontroller {
 	TextField psvalue = new TextField();
 	@FXML
 	Button psave = new Button();
-	public void invsave(ActionEvent event)
+	@FXML
+			ComboBox<String> cmbcat;
+	@FXML
+	Label prolblmsg = new Label();
+	@FXML 
+Button btnprocan = new Button();
+	
+	PreparedStatement statement = null;
+	
+	 /**
+	     * Here we will define the method that builds the List used by the ComboBox
+	     */
+	    private List<String> getData() {
+
+
+	        // Define the data you will be returning, in this case, a List of Strings for the ComboBox
+	        List<String> options = new ArrayList<>();
+	        
+	        try {
+	        	Class.forName("org.sqlite.JDBC");
+				Connection conn = DriverManager.getConnection("jdbc:sqlite:testdb.sqlite");
+	            String query = "select catname from tbcat order by catname ASC";
+	            PreparedStatement statement = conn.prepareStatement(query);
+	            
+	            ResultSet set = statement.executeQuery();
+	            
+	            while (set.next()) {
+	                options.add(set.getString("catname"));
+	               
+	            }
+	            
+	            statement.close();
+	            set.close();
+	          
+	            // Return the List
+	            return options;
+	            
+
+	        } catch (ClassNotFoundException | SQLException ex) {
+	            return null;
+	        }
+	       
+	    }
+	   
+	    
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		cmbcat.setItems(FXCollections.observableArrayList(getData()));
+		 cmbcat.setEditable(true);
+         TextFields.bindAutoCompletion(cmbcat.getEditor(), cmbcat.getItems());
+	}
+	public void prosave(ActionEvent event)
 	{
-		Connection conection = SqliteConnection.Connector();
-		  String query="INSERT INTO item (name,address,phone,email) values (?,?,?,?);";
+		prolblmsg.setText("Inserted successfully");
+	}
+	/* close code */
+	public void procan(ActionEvent evnet)
+	{
+		   Stage stage = (Stage) btnprocan.getScene().getWindow();
+		   stage.close();
 	}
 }
